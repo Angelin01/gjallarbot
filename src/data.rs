6842c9e5b83754edacf5ef3437ed5f87c78b5d3b
angelo <angeloborsoiross@hotmail.com>
2024-10-20 18:36:17 -0300

    Create mock_data test function

--- src/data.rs
+++ src/data.rs
@@ -17,3 +17,22 @@ pub type BotData = Arc<RwLock<PersistentJson<Data>>>;
 pub type BotError = Box<dyn std::error::Error + Send + Sync>;
 
 pub type Context<'a> = poise::Context<'a, BotData, BotError>;
+
+#[cfg(test)]
+pub mod tests {
+	use super::*;
+	use serde_json::Value;
+	use std::io::Write;
+	use tempfile::NamedTempFile;
+	pub fn mock_data(initial_data: Option<Value>) -> BotData {
+		let mut temp_file = NamedTempFile::new().unwrap();
+		if let Some(data) = initial_data {
+			temp_file.write_all(serde_json::to_string(&data).unwrap().as_bytes()).unwrap();
+			temp_file.flush().unwrap();
+		}
+
+		let persistent_data = PersistentJson::new(temp_file.path()).unwrap();
+
+		Arc::new(RwLock::new(persistent_data))
+	}
+}
