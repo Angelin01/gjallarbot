7fa1b6920f2a4a779ddf8e0ecd228b9ea4057721
angelo <angeloborsoiross@hotmail.com>
2024-10-22 20:30:36 -0300

    Prettify debug register command

--- src/commands/register.rs
+++ src/commands/register.rs
@@ -1,6 +1,7 @@
 use crate::data::{Context, BotError};
 
-#[poise::command(slash_command)]
+/// Debugging: register commands
+#[poise::command(slash_command, owners_only)]
 pub async fn register(ctx: Context<'_>) -> Result<(), BotError> {
 	poise::builtins::register_application_commands_buttons(ctx).await?;
 	Ok(())
