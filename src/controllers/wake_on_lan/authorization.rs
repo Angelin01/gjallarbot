c428e077b865fbd3b9ff6ecfabb3a4c160ac9f5e
angelo <angeloborsoiross@hotmail.com>
2025-02-11 21:00:08 -0300

    Reimplement role permission controller

--- src/controllers/wake_on_lan/authorization.rs
+++ src/controllers/wake_on_lan/authorization.rs
@@ -1,11 +1,12 @@
 use super::{get_machine_info_mut, MachineError};
 use crate::data::BotData;
-use serenity::all::{UserId};
+use serenity::all::{RoleId, UserId};
 use thiserror::Error;
 
 #[derive(Debug, PartialEq)]
 pub enum Entity {
 	User(UserId),
+	Role(RoleId),
 }
 
 #[derive(Debug, Error, PartialEq)]
@@ -67,6 +68,46 @@ pub async fn revoke_user(
 		Err(RemovePermissionError::AlreadyNotAuthorized {
 			machine_name: machine_name.into(),
 			entity: Entity::User(user_id),
+		})
+	}
+}
+
+pub async fn permit_role(
+	data: &BotData,
+	machine_name: &str,
+	role_id: RoleId,
+) -> Result<(), AddPermissionError> {
+	let mut lock = data.write().await;
+	let mut data_write = lock.write();
+
+	let machine_info = get_machine_info_mut(&mut data_write, machine_name).await?;
+
+	if machine_info.authorized_roles.insert(role_id) {
+		Ok(())
+	} else {
+		Err(AddPermissionError::AlreadyAuthorized {
+			machine_name: machine_name.into(),
+			entity: Entity::Role(role_id),
+		})
+	}
+}
+
+pub async fn revoke_role(
+	data: &BotData,
+	machine_name: &str,
+	role_id: RoleId,
+) -> Result<(), RemovePermissionError> {
+	let mut lock = data.write().await;
+	let mut data_write = lock.write();
+
+	let machine_info = get_machine_info_mut(&mut data_write, machine_name).await?;
+
+	if machine_info.authorized_roles.remove(&role_id) {
+		Ok(())
+	} else {
+		Err(RemovePermissionError::AlreadyNotAuthorized {
+			machine_name: machine_name.into(),
+			entity: Entity::Role(role_id),
 		})
 	}
 }
@@ -207,5 +248,138 @@ mod tests {
 		assert!(!data.read().await.wake_on_lan["ExistingMachine"]
 			.authorized_users
 			.contains(&UserId::new(12345678901234567)));
+	}
+
+	#[tokio::test]
+	async fn given_nonexistent_machine_then_permit_role_returns_error_and_does_not_modify_data() {
+		let data = mock_data(None);
+
+		let result = permit_role(&data, "NonExistentMachine", RoleId::new(98765432109876543)).await;
+
+		assert_eq!(
+			result,
+			Err(AddPermissionError::Machine(
+				MachineError::NonExistentMachine {
+					machine_name: "NonExistentMachine".to_string()
+				}
+			))
+		);
+		assert!(data.read().await.wake_on_lan.is_empty());
+	}
+
+	#[tokio::test]
+	async fn given_already_authorized_role_then_permit_role_returns_error_and_does_not_modify_data()
+	{
+		let data = mock_data(Some(json!({
+			"wake_on_lan": {
+				"ExistingMachine": {
+					"mac": [1, 2, 3, 4, 5, 6],
+					"authorized_users": [],
+					"authorized_roles": [98765432109876543u64]
+				}
+			}
+		})));
+
+		let result = permit_role(&data, "ExistingMachine", RoleId::new(98765432109876543)).await;
+
+		assert_eq!(
+			result,
+			Err(AddPermissionError::AlreadyAuthorized {
+				machine_name: "ExistingMachine".to_string(),
+				entity: Entity::Role(RoleId::new(98765432109876543))
+			})
+		);
+		assert_eq!(
+			data.read().await.wake_on_lan["ExistingMachine"]
+				.authorized_roles
+				.len(),
+			1
+		);
+	}
+
+	#[tokio::test]
+	async fn given_new_role_then_permit_role_returns_success_and_adds_role() {
+		let data = mock_data(Some(json!({
+			"wake_on_lan": {
+				"ExistingMachine": {
+					"mac": [1, 2, 3, 4, 5, 6],
+					"authorized_users": [],
+					"authorized_roles": []
+				}
+			}
+		})));
+
+		let result = permit_role(&data, "ExistingMachine", RoleId::new(98765432109876543)).await;
+
+		assert_eq!(result, Ok(()));
+		assert!(data.read().await.wake_on_lan["ExistingMachine"]
+			.authorized_roles
+			.contains(&RoleId::new(98765432109876543)));
+	}
+
+	#[tokio::test]
+	async fn given_nonexistent_machine_then_revoke_role_returns_error_and_does_not_modify_data() {
+		let data = mock_data(None);
+
+		let result = revoke_role(&data, "NonExistentMachine", RoleId::new(98765432109876543)).await;
+
+		assert_eq!(
+			result,
+			Err(RemovePermissionError::Machine(
+				MachineError::NonExistentMachine {
+					machine_name: "NonExistentMachine".to_string()
+				}
+			))
+		);
+		assert!(data.read().await.wake_on_lan.is_empty());
+	}
+
+	#[tokio::test]
+	async fn given_non_authorized_role_then_revoke_role_returns_error_and_does_not_modify_data() {
+		let data = mock_data(Some(json!({
+			"wake_on_lan": {
+				"ExistingMachine": {
+					"mac": [1, 2, 3, 4, 5, 6],
+					"authorized_users": [],
+					"authorized_roles": [12345678901234567u64]
+				}
+			}
+		})));
+
+		let result = revoke_role(&data, "ExistingMachine", RoleId::new(98765432109876543)).await;
+
+		assert_eq!(
+			result,
+			Err(RemovePermissionError::AlreadyNotAuthorized {
+				machine_name: "ExistingMachine".to_string(),
+				entity: Entity::Role(RoleId::new(98765432109876543))
+			})
+		);
+		assert_eq!(
+			data.read().await.wake_on_lan["ExistingMachine"]
+				.authorized_roles
+				.len(),
+			1
+		);
+	}
+
+	#[tokio::test]
+	async fn given_authorized_role_then_revoke_role_returns_success_and_removes_role() {
+		let data = mock_data(Some(json!({
+			"wake_on_lan": {
+				"ExistingMachine": {
+					"mac": [1, 2, 3, 4, 5, 6],
+					"authorized_users": [],
+					"authorized_roles": [98765432109876543u64]
+				}
+			}
+		})));
+
+		let result = revoke_role(&data, "ExistingMachine", RoleId::new(98765432109876543)).await;
+
+		assert_eq!(result, Ok(()));
+		assert!(!data.read().await.wake_on_lan["ExistingMachine"]
+			.authorized_roles
+			.contains(&RoleId::new(98765432109876543)));
 	}
 }
