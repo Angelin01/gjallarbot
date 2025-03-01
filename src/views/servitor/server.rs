084c6f8ec2ba3d8eab72fcecc5f74c5e2a44b989
Angelo Ross <angelo.ross@objective.com.br>
2025-03-01 19:13:16 -0300

    Implement servitor view for remove server

--- src/views/servitor/server.rs
+++ src/views/servitor/server.rs
@@ -1,6 +1,6 @@
-use crate::controllers::servitor::server::AddServerError;
-use serenity::builder::CreateEmbed;
+use crate::controllers::servitor::server::{AddServerError, RemoveServerError};
 use crate::embeds;
+use serenity::builder::CreateEmbed;
 
 pub fn add_server_embed(
 	result: Result<(), AddServerError>,
@@ -17,6 +17,23 @@ pub fn add_server_embed(
 			AddServerError::InvalidServitor { .. } => embeds::error("Invalid Servitor", format!("There is no such servitor instance with name {servitor}")),
 			AddServerError::Server(_) => embeds::error("Duplicate name", format!("A servitor server with name {server_name} already exists, try a different name"))
 		}
+	}
+}
+
+pub fn remove_server_embed(
+	result: Result<(), RemoveServerError>,
+	server_name: &str,
+) -> CreateEmbed {
+	match result {
+		Ok(_) => embeds::success("Success", "Successfully removed server").field(
+			"Name",
+			server_name,
+			true,
+		),
+		Err(_) => embeds::error(
+			"Invalid Server",
+			format!("No servitor server with name {server_name} exists"),
+		),
 	}
 }
 
@@ -71,6 +88,35 @@ mod tests {
 			.field("Name", "SomeServer", true)
 			.field("Servitor", "foo", true)
 			.field("Unit Name", "bar", true);
+
+		assert_eq!(embed, expected_embed);
+	}
+
+	#[test]
+	fn given_remove_server_with_does_not_exist_error_then_reply_with_non_existing_server() {
+		let result = Err(RemoveServerError::Server(ServerError::DoesNotExist {
+			server_name: "NonExistingServer".to_string(),
+		}));
+
+		let embed = remove_server_embed(result, "NonExistingServer");
+
+		let expected_embed = CreateEmbed::default()
+			.title(":x: Invalid Server")
+			.colour(Colour(0xdd2e44))
+			.description("No servitor server with name NonExistingServer exists");
+
+		assert_eq!(embed, expected_embed);
+	}
+
+	#[test]
+	fn given_successful_remove_server_then_reply_with_success_info() {
+		let embed = remove_server_embed(Ok(()), "SomeServer");
+
+		let expected_embed = CreateEmbed::default()
+			.title(":white_check_mark: Success")
+			.colour(Colour(0x77b255))
+			.description("Successfully removed server")
+			.field("Name", "SomeServer", true);
 
 		assert_eq!(embed, expected_embed);
 	}
