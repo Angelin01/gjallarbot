704e1cc50bd8d1e01498a14f9dae09faca9a2a22
Angelo Ross <angelo.ross@objective.com.br>
2025-03-01 16:38:33 -0300

    Implement servitor view for add server

---
+++ src/views/servitor/server.rs
@@ -0,0 +1,77 @@
+use crate::controllers::servitor::server::AddServerError;
+use serenity::builder::CreateEmbed;
+use crate::embeds;
+
+pub fn add_server_embed(
+	result: Result<(), AddServerError>,
+	server_name: &str,
+	servitor: &str,
+	unit_name: &str,
+) -> CreateEmbed {
+	match result {
+		Ok(_) => embeds::success("Success", "Successfully added new server")
+			.field("Name", server_name, true)
+			.field("Servitor", servitor, true)
+			.field("Unit Name", unit_name, true),
+		Err(e) => match e {
+			AddServerError::InvalidServitor { .. } => embeds::error("Invalid Servitor", format!("There is no such servitor instance with name {servitor}")),
+			AddServerError::Server(_) => embeds::error("Duplicate name", format!("A servitor server with name {server_name} already exists, try a different name"))
+		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::controllers::servitor::ServerError;
+	use serenity::all::Colour;
+
+	#[test]
+	fn given_add_server_error_with_invalid_servitor_then_reply_with_invalid_servitor() {
+		let result = Err(AddServerError::InvalidServitor {
+			name: "foo".to_string(),
+		});
+
+		let embed = add_server_embed(result, "SomeServer", "foo", "bar");
+
+		let expected_embed = CreateEmbed::default()
+			.title(":x: Invalid Servitor")
+			.colour(Colour(0xdd2e44))
+			.description("There is no such servitor instance with name foo");
+
+		assert_eq!(embed, expected_embed);
+	}
+
+	#[test]
+	fn given_add_server_error_with_existing_server_then_reply_with_error_duplicate_name() {
+		let result = Err(AddServerError::Server(ServerError::AlreadyExists {
+			server_name: "SomeServer".to_string(),
+		}));
+
+		let embed = add_server_embed(result, "SomeServer", "foo", "bar");
+
+		let expected_embed = CreateEmbed::default()
+			.title(":x: Duplicate name")
+			.colour(Colour(0xdd2e44))
+			.description(
+				"A servitor server with name SomeServer already exists, try a different name",
+			);
+
+		assert_eq!(embed, expected_embed);
+	}
+
+	#[test]
+	fn given_successful_add_server_then_reply_with_success_info() {
+		let embed = add_server_embed(Ok(()), "SomeServer", "foo", "bar");
+
+		let expected_embed = CreateEmbed::default()
+			.title(":white_check_mark: Success")
+			.colour(Colour(0x77b255))
+			.description("Successfully added new server")
+			.field("Name", "SomeServer", true)
+			.field("Servitor", "foo", true)
+			.field("Unit Name", "bar", true);
+
+		assert_eq!(embed, expected_embed);
+	}
+}
