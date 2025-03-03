5e35f8463f113c75c6a48d2eb338fb1d67472426
Angelo Ross <angelo.ross@objective.com.br>
2025-03-02 21:57:04 -0300

    Extract author mock functions

--- src/controllers.rs
+++ src/controllers.rs
@@ -8,3 +8,26 @@ pub enum DiscordEntity {
 	User(UserId),
 	Role(RoleId),
 }
+
+#[cfg(test)]
+pub mod tests {
+	use serenity::all::{Member, RoleId, User, UserId};
+
+	pub fn mock_author_dms(id: UserId) -> User {
+		let mut user = User::default();
+		user.id = id;
+		user.name = "mock_author".to_string();
+		user
+	}
+
+	pub fn mock_author_guild(id: UserId, roles: Vec<RoleId>) -> User {
+		let mut user = mock_author_dms(id);
+
+		let mut member = Member::default();
+		member.roles = roles;
+
+		user.member = Some(Box::new(member.into()));
+
+		user
+	}
+}
